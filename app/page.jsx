55ee61ee2389439686a7e13041134e842a202f67
Ali <ali.tradding021@gmail.com>
2024-12-20 23:23:46 +0330

    commit

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import "./globals.css"
 import  Video  from "@/components/video";
 import ProductShop from "@/app/product/page";
-import ImageSlider from '@/components/imageSlider';
 
 
 
@@ -11,7 +10,6 @@ export default function page() {
   return (
     <section className="w-full">
       <Video/>
-      <ImageSlider/>
       <ProductShop/>
     </section>
   )
